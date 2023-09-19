88d303bd79e1d6e1e3b847fc79f5e951c74745ad
Kingsley320 <75090590+Kingsley320@users.noreply.github.com>
2023-09-19 17:26:07 +0100

    new pages

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import Footer from "../components/Footer"
 import Navigation from "../components/Navigation"
+import SignUp from "../components/SignUp"
 
 function HomePage(){
 
     return(
         <>
         <Navigation/>
+        <SignUp/>
         {/* <h1>Hi Lemmy</h1> */}
         <Footer/>
         </>
