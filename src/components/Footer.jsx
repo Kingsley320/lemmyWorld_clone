51525c3b9c7ae99235bb1afc67d2da55b36106b3
Kingsley320 <75090590+Kingsley320@users.noreply.github.com>
2023-09-21 23:40:37 +0100

    changed position to correct overlapping problems

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 function Footer(){
     return(
         <>
-            <ul className="text-zinc-400 w-full relative right-0 bottom-0 p-4 pb-4 leading-[2.3rem] font-extralight font-sans md:gap-4 md:flex md:justify-end md:right-14">
+            <ul className="text-zinc-400 w-full relative right-0 bottom-0 p-4 pb-4 leading-[2.3rem] font-extralight font-sans md:gap-4 md:flex md:justify-end ">
                 <li>BE: 0.18.4</li>
                 <li>Modlog</li>
                 <li>Legal</li>
