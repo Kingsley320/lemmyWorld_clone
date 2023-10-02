491f97c0189b6b67362b78c8f6f4ed4ef8fd1c88
Kingsley320 <75090590+Kingsley320@users.noreply.github.com>
2023-10-02 23:22:51 +0100

    sorry, this is the post component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,10 +3,11 @@ import defaultLogo from '../assets/defaultLogo.png'
 import samplePost from '../assets/sample_post_img.webp'
 
 
-export default function Feed(){
+export default function Feed(props){
+
     return(
-        <div className="min-h-screen py-2">
-            <Post defaultUser={defaultLogo} username={"@kingsley"} catLogo={defaultLogo} category={"Technology"} language={"English"} dateCreated={"3 days ago"} title={"Inside ShadowDragon, The Tool That Lets ICE Monitor Pregnancy Tracking Sites and Fortnite Players"} url={"www.404media.co"} image={samplePost} comments={"230"} likes={"167"} dislikes={"19"}/>
+        <div className="py-2">
+            <Post defaultUser={defaultLogo} username={"@kingsley"} catLogo={defaultLogo} category={props.community} language={props.language} dateCreated={"3 days ago"} title={props.title} url={props.url} image={props.image } comments={"230"} likes={"167"} dislikes={"19"}/>
         </div>
     )
 }
